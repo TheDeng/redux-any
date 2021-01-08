8739d65fd3bd6ffc27d2e9121c0cab2e44e06b8d
TheDeng <dengjiale1996@163.com>
2021-01-08 18:40:03 +0800

    finish nested array iteration

--- README.md
+++ README.md
@@ -1,6 +1,7 @@
 # redux-any
 # a middleware for processing action like function,promise,and array
-Our middleware is based on redux-promise and redux-thunk .Our middlewrae enhances and combines these two middlewares so that it can processes many kinds of actions like function,promise,and array
+
+### Our middleware is based on redux-promise and redux-thunk .Our middlewrae enhances and combines these two middlewares so that it can processes many kinds of actions like function,promise,and array
 # Usage:
 
-npm i redux-any
+npm i redux-any --save
--- src/index.js
+++ src/index.js
@@ -1,31 +1,37 @@
 import isPromise from 'is-promise';
 import { isFSA } from 'flux-standard-action';
 
-export default function promiseMiddleware({ dispatch }) {
+export default function promiseMiddleware({ dispatch, getState }) {
     return next => action => {
-        if (Array.isArray(action)) {
-            action.map(v => {
-                process(dispatch, next, v)
-            })
-        } else {
-            process(dispatch, next, action)
-        }
+
+        return process(dispatch, getState, next, action)
+
     };
 }
-function process(dispatch, next, action) {
-    if (typeof action === 'function') {
-        return action(dispatch, getState);
-    }
-    if (!isFSA(action)) {
-        return isPromise(action) ? action.then(dispatch) : next(action);
+function process(dispatch, getState, next, action) {
+    if (Array.isArray(action)) {
+        action.map(v => {
+            return process(dispatch, getState, next, v)
+        })
+    } else {
+
+
+        if (typeof action === 'function') {
+            return action(dispatch, getState);
+        }
+        if (!isFSA(action)) {
+            return isPromise(action) ? action.then(dispatch) : next(action);
+        }
+
+        return isPromise(action.payload)
+            ? action.payload
+                .then(result => dispatch({ ...action, payload: result }))
+                .catch(error => {
+                    dispatch({ ...action, payload: error, error: true });
+                    return Promise.reject(error);
+                })
+            : next(action);
+
     }
 
-    return isPromise(action.payload)
-        ? action.payload
-            .then(result => dispatch({ ...action, payload: result }))
-            .catch(error => {
-                dispatch({ ...action, payload: error, error: true });
-                return Promise.reject(error);
-            })
-        : next(action);
 }
